96a1ddf395b88e21276d6561ee224f43f5335ed7
MEGATOK DEV <megatokcc@gmail.com>
2023-06-22 15:52:52 -0300

    Fix Download

--- src/components/defaults/CardVideo.jsx
+++ src/components/defaults/CardVideo.jsx
@@ -15,7 +15,7 @@ function CardVideo({ Title, Thumbnail, Formats }) {
          <h1 className={"text-ColorTwo text-xl text-center col-span-2 underline"}>{Title}</h1> 
          <span className='pl-4 col-span-2 text-lg font-light text-ColorTwo mt-1'>Correto?</span>
          <div className='flex justify-around items-center mt-2'>
-           <a href={`api/download?url=${encodeURIComponent(Formats[0].url)}&title=${encodeURIComponent(Title)}`}>
+           <a href={`api/download?url=${encodeURIComponent(Formats[0].url)}&title=${encodeURIComponent(Title)}`} className={`bg-ColorTree p-[12px] text-ColorTwo rounded-lg hover:scale-x-110 transition-all`}>
                 Sim
            </a>
            <ButtonStylized onClick={() => console.log("oi")} padding={"12px"}>
