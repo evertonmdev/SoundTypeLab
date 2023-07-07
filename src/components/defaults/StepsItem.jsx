ed443703f325740cff4af3b8f863b7bcb02c9633
AnakonStar <enzoh3h7@gmail.com>
2023-07-06 22:07:32 -0300

    Correção de display e limpando código

--- src/components/defaults/StepsItem.jsx
+++ src/components/defaults/StepsItem.jsx
@@ -2,16 +2,14 @@ import Image from 'next/image';
 
 export default function StepsItem({ imgUrl, text, title }) {
     return (
-        <section>
-            <details>
-                <summary>
-                    {title}
-                </summary>
-                <Image src={imgUrl} className='image' />
-                <span>
-                    {text}
-                </span>
-            </details>
-        </section>
+        <details>
+            <summary>
+                {title}
+            </summary>
+            <Image src={imgUrl} />
+            <span>
+                {text}
+            </span>
+        </details>
     )
 }
