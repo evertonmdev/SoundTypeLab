3ea7baea3598137cd1178a4bf4aa69ff5628654b
AnakonStar <107694439+AnakonStar@users.noreply.github.com>
2023-07-12 00:54:36 -0300

    Update LogOutButton.jsx

--- src/components/defaults/LogOutButton.jsx
+++ src/components/defaults/LogOutButton.jsx
@@ -2,8 +2,8 @@
 import { LogOut } from 'lucide-react';
 import { signOut } from 'next-auth/react';
 
-export default function LogOutButton() {
+export default function LogOutButton({size}) {
     return (
-        <button className='logout-button' onClick={() => signOut()}><LogOut/> Sair</button>
+        <button className='logout-button' onClick={() => signOut()}><LogOut size={size}/> Sair</button>
     )
 }
